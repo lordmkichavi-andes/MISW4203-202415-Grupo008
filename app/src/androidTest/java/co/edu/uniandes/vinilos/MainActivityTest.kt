2ccc021d2e8caa400cb0e7743701eb768af52c0d
Javier <jd.fajardor1@uniandes.edu.co>
2024-10-26 09:23:02 -0500

    adicion de codigo base

--- app/src/androidTest/java/co/edu/uniandes/vinilos/MainActivityTest.kt
+++ app/src/androidTest/java/co/edu/uniandes/vinilos/MainActivityTest.kt
@@ -1,7 +1,9 @@
 package co.edu.uniandes.vinilos
 
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
 
