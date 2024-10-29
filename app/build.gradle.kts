67020fcfd1e31672c6bb03b3ee5ef98a56c2507d
Renvel19 <af.renteria2@uniandes.edu.co>
2024-10-29 16:43:43 -0500

    add form

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.volley)
     implementation(libs.gson)
     implementation(libs.androidx.navigation.compose)
+    implementation("androidx.compose.material3:material3:<version>")
+    implementation("androidx.compose.material:material-icons-extended:<version>")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
