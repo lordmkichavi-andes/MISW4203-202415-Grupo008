9c2ef469963965cc768803138928557ad97b4e72
Javier <jd.fajardor1@uniandes.edu.co>
2024-11-02 15:21:22 -0500

    mejora de las pruebas e2e en los estados

--- app/src/androidTest/java/co/edu/uniandes/vinilos/GetAlbumesScreenE2E.kt
+++ app/src/androidTest/java/co/edu/uniandes/vinilos/GetAlbumesScreenE2E.kt
@@ -2,14 +2,15 @@ package co.edu.uniandes.vinilos
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+import kotlin.system.measureTimeMillis
 
 @RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class GetAlbumCatalogE2ETest {
 
     @get:Rule
@@ -17,36 +18,50 @@ class GetAlbumCatalogE2ETest {
 
     @Before
     fun setUp() {
-        composeTestRule.waitUntil(timeoutMillis = 5000) {
-            composeTestRule.onAllNodes(hasText("Selecciona tu tipo de usuario para comenzar:")).fetchSemanticsNodes().isNotEmpty()
+        composeTestRule.activityRule.scenario.recreate()
+        composeTestRule.activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
+
+        composeTestRule.waitUntil(timeoutMillis = 10000) {
+            composeTestRule.onAllNodes(hasText("Selecciona tu tipo de usuario para comenzar:"))
+                .fetchSemanticsNodes().isNotEmpty()
         }
 
+        composeTestRule.onNodeWithText("Selecciona tu tipo de usuario para comenzar:")
+            .assertIsDisplayed()
         composeTestRule.onNodeWithText("Usuario").performClick()
     }
 
     @After
     fun tearDown() {
-        composeTestRule.activityRule.scenario.recreate()
+        Thread.sleep(1000)
+        composeTestRule.activityRule.scenario.close()
     }
 
     @Test
     fun testAlbumCatalogIsDisplayedForVisitorProfile() {
-        composeTestRule.waitUntil(timeoutMillis = 10000) {
-            composeTestRule.onAllNodesWithTag("AlbumItem").fetchSemanticsNodes().isNotEmpty()
-        }
+        val timeTaken = measureTimeMillis {
+            composeTestRule.waitUntil(timeoutMillis = 10000) {
+                composeTestRule.onAllNodesWithTag("AlbumItem").fetchSemanticsNodes().isNotEmpty()
+            }
 
-        composeTestRule.onAllNodesWithTag("AlbumItem")
-            .onFirst()
-            .assertIsDisplayed()
+            composeTestRule.onAllNodesWithTag("AlbumItem")
+                .onFirst()
+                .assertIsDisplayed()
+        }
+        println("Tiempo para testAlbumCatalogIsDisplayedForVisitorProfile: $timeTaken ms")
     }
 
     @Test
     fun testAlbumLoadingMessageIsDisplayed() {
-        composeTestRule.waitUntil(timeoutMillis = 5000) {
-            composeTestRule.onAllNodesWithText("Cargando tus álbumes de vinilos favoritos.").fetchSemanticsNodes().isNotEmpty()
-        }
+        val timeTaken = measureTimeMillis {
+            composeTestRule.waitUntil(timeoutMillis = 5000) {
+                composeTestRule.onAllNodesWithText("Cargando tus álbumes de vinilos favoritos.")
+                    .fetchSemanticsNodes().isNotEmpty()
+            }
 
-        composeTestRule.onNodeWithText("Cargando tus álbumes de vinilos favoritos.")
-            .assertIsDisplayed()
+            composeTestRule.onNodeWithText("Cargando tus álbumes de vinilos favoritos.")
+                .assertIsDisplayed()
+        }
+        println("Tiempo para testAlbumLoadingMessageIsDisplayed: $timeTaken ms")
     }
 }
