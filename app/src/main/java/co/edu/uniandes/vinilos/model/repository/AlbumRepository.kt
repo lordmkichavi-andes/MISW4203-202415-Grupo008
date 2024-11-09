c9a8943973924a4c013ef63651dded435099faa1
Renvel19 <af.renteria2@uniandes.edu.co>
2024-11-09 09:27:12 -0500

    add cache validation for albums (#57)

--- app/src/main/java/co/edu/uniandes/vinilos/model/repository/AlbumRepository.kt
+++ app/src/main/java/co/edu/uniandes/vinilos/model/repository/AlbumRepository.kt
@@ -1,30 +1,43 @@
 package co.edu.uniandes.vinilos.model.repository
 
+import android.content.Context
 import android.util.Log
+import androidx.compose.ui.platform.LocalContext
 import co.edu.uniandes.vinilos.model.models.Album
 import co.edu.uniandes.vinilos.model.providers.AlbumProvider
+import co.edu.uniandes.vinilos.network.CacheManager
 
 class AlbumRepository(val albumProvider: AlbumProvider) {
 
-    fun addAbum(album: Album, onSuccess: () -> Unit, onError: (String) -> Unit) {
+    fun addAbum(context: Context, album: Album, onSuccess: (Album) -> Unit, onError: (String) -> Unit) {
         Log.d("AlbumRepository", "Enviando solicitud para agregar álbum: ${album.name}")
-        albumProvider.addAbum(album, {
+        albumProvider.addAbum(album, {registeredAlbum ->
             Log.d("AlbumRepository", "Álbum agregado exitosamente.")
-            onSuccess()
+            registeredAlbum.id?.let { CacheManager.getInstance(context).addAlbum(it, registeredAlbum) }
+            onSuccess(registeredAlbum)
         }, { error ->
             Log.e("AlbumRepository", "Error al agregar álbum: $error")
             onError(error)
         })
     }
 
-    fun getAlbums(onSuccess: (List<Album>) -> Unit, onError: (String) -> Unit) {
-        Log.d("AlbumRepository", "Iniciando solicitud para obtener álbumes.")
-        albumProvider.getAlbums({ albumList ->
-            Log.d("AlbumRepository", "Álbumes obtenidos exitosamente: ${albumList.size} álbumes.")
-            onSuccess(albumList)
-        }, { error ->
-            Log.e("AlbumRepository", "Error al obtener álbumes: $error")
-            onError(error)
-        })
+    fun getAlbums( context: Context, onSuccess: (List<Album>) -> Unit, onError: (String) -> Unit) {
+        val cacheAlbums = CacheManager.getInstance(context).getAlbums()
+        if (cacheAlbums.isNullOrEmpty()) {
+            Log.d("AlbumRepository", "Iniciando solicitud para obtener álbumes.")
+            albumProvider.getAlbums({ albumList ->
+                Log.d("AlbumRepository", "Álbumes obtenidos exitosamente: ${albumList.size} álbumes.")
+                CacheManager.getInstance(context).saveAlbums(albumList)
+                onSuccess(albumList)
+            }, { error ->
+                Log.e("AlbumRepository", "Error al obtener álbumes: $error")
+                onError(error)
+            })
+        } else {
+            Log.d("AlbumRepository", "Obteniendo albums desde memoria caché.")
+            onSuccess(cacheAlbums)
+        }
+
+
     }
 }
