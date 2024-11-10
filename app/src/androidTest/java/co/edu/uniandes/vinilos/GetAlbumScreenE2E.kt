9a652bb17224e56400578519bcac02eb4070b0ea
Margarita Villafañe <m.villafanep@uniandes.edu.co>
2024-11-10 18:04:33 -0500

    Feature/get album e2e (#60)
    * feat: e2e for detail album
    
    * rename file
    
    ---------
    
    Co-authored-by: Margarita Villafañe <mporras@fundaciongruposocial.co>

---
+++ app/src/androidTest/java/co/edu/uniandes/vinilos/GetAlbumScreenE2E.kt
@@ -0,0 +1,55 @@
+package co.edu.uniandes.vinilos
+
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.*
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+import kotlin.system.measureTimeMillis
+
+@RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class GetAlbumScreenE2ETest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setUp() {
+        composeTestRule.activityRule.scenario.recreate()
+        composeTestRule.activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
+
+        composeTestRule.waitUntil(timeoutMillis = 10000) {
+            composeTestRule.onAllNodes(hasText("Selecciona tu tipo de usuario para comenzar:"))
+                .fetchSemanticsNodes().isNotEmpty()
+        }
+
+        composeTestRule.onNodeWithText("Coleccionista").performClick()
+    }
+
+    @After
+    fun tearDown() {
+        Thread.sleep(1000)
+        composeTestRule.activityRule.scenario.close()
+    }
+
+    @Test
+    fun testAddTrackButtonIsDisplayedForCollectorProfile() {
+        val timeTaken = measureTimeMillis {
+            composeTestRule.waitUntil(timeoutMillis = 10000) {
+                composeTestRule.onAllNodesWithTag("AlbumItem").fetchSemanticsNodes().isNotEmpty()
+            }
+
+            composeTestRule.onAllNodesWithTag("AlbumItem").onFirst().performClick()
+
+            composeTestRule.waitUntil(timeoutMillis = 10000) {
+                composeTestRule.onAllNodesWithTag("AgregarTrackButton").fetchSemanticsNodes().isNotEmpty()
+            }
+
+            composeTestRule.onNodeWithTag("AgregarTrackButton").assertIsDisplayed()
+        }
+        println("Tiempo para testAddTrackButtonIsDisplayedForCollectorProfile: $timeTaken ms")
+    }
+}
