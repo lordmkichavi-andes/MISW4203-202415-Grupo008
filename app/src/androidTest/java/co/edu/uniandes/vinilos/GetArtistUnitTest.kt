7d628c572d6648e20a78099bc98041447f439124
Margarita Villafañe <m.villafanep@uniandes.edu.co>
2024-11-17 16:10:23 -0500

    Develop (#67)
    * adicion de pruebas unitarias del listado de artistas y actualizacion de pruebas e2e del mismo (#64)
    
    * Feature/get artist (#65)
    
    * adicion de pruebas unitarias del listado de artistas y actualizacion de pruebas e2e del mismo
    
    * adicion de test unitarios de detalle de listado de artista
    
    * actualizacion de test unitario
    
    * Get artist e2e (#66)
    
    * feat: e2e de detalle de artista
    
    * comment: listado de coleccionistas en el nav
    
    ---------
    
    Co-authored-by: Margarita Villafañe <mporras@fundaciongruposocial.co>
    
    ---------
    
    Co-authored-by: Renvel19 <af.renteria2@uniandes.edu.co>
    Co-authored-by: Javier Dario Fajardo Rodriguez <jd.fajardor1@uniandes.edu.co>
    Co-authored-by: Margarita Villafañe <mporras@fundaciongruposocial.co>

---
+++ app/src/androidTest/java/co/edu/uniandes/vinilos/GetArtistUnitTest.kt
@@ -0,0 +1,49 @@
+package co.edu.uniandes.vinilos
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import co.edu.uniandes.vinilos.screens.ArtistItem
+import co.edu.uniandes.vinilos.screens.formatBirthDate
+import co.edu.uniandes.vinilos.viewmodel.Artist
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ArtistListScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testArtistItemDisplaysCorrectly() {
+        composeTestRule.setContent {
+            ArtistItem(
+                artist = Artist(
+                    id = 1,
+                    name = "Nombre del Artista",
+                    birthDate = "1990-01-01T00:00:00.000Z",
+                    image = "https://example.com/image.jpg"
+                ),
+                onClick = {}
+            )
+        }
+
+        composeTestRule.onNodeWithText("Nombre del Artista").assertExists()
+        composeTestRule.onNodeWithText("01/01/1990").assertExists()
+    }
+
+    @Test
+    fun testFormatBirthDateReturnsCorrectly() {
+        val formattedDate = formatBirthDate("1990-01-01T00:00:00.000Z")
+        Assert.assertEquals("01/01/1990", formattedDate)
+
+        val unknownDate = formatBirthDate(null)
+        Assert.assertEquals("Fecha desconocida", unknownDate)
+
+        val invalidDate = formatBirthDate("invalid-date")
+        Assert.assertEquals("Fecha desconocida", invalidDate)
+    }
+}
