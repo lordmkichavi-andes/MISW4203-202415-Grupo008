a59f6afd62379e29543ce93b9a852c951bebc095
Javier <jd.fajardor1@uniandes.edu.co>
2024-11-21 22:45:08 -0500

    adicion de pruebas aleatorias y optimizacion de codigo para artistas

--- app/src/androidTest/java/co/edu/uniandes/vinilos/GetArtistUnitTest.kt
+++ app/src/androidTest/java/co/edu/uniandes/vinilos/GetArtistUnitTest.kt
@@ -10,6 +10,7 @@ import org.junit.Assert
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.random.Random
 
 @RunWith(AndroidJUnit4::class)
 class ArtistListScreenTest {
@@ -45,5 +46,53 @@ class ArtistListScreenTest {
 
         val invalidDate = formatBirthDate("invalid-date")
         Assert.assertEquals("Fecha desconocida", invalidDate)
+    }
+
+    @Test
+    fun testArtistItemWithRandomData() {
+        val randomName = generateRandomString(10)
+        val randomDate = generateRandomDate()
+        val randomImage = "https://example.com/${generateRandomString(5)}.jpg"
+
+        composeTestRule.setContent {
+            ArtistItem(
+                artist = Artist(
+                    id = Random.nextInt(1, 1000),
+                    name = randomName,
+                    birthDate = randomDate,
+                    image = randomImage
+                ),
+                onClick = {}
+            )
+        }
+
+        composeTestRule.onNodeWithText(randomName).assertExists()
+        composeTestRule.onNodeWithText(formatBirthDate(randomDate)).assertExists()
+    }
+
+    private fun generateRandomString(length: Int): String {
+        val chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        return (1..length)
+            .map { chars.random() }
+            .joinToString("")
+    }
+
+    private fun generateRandomDate(): String {
+        val validDates = listOf(
+            "2020-12-12T12:00:00.000Z",
+            "1990-01-01T00:00:00.000Z",
+            "2001-09-11T08:46:00.000Z"
+        )
+        val invalidDates = listOf(
+            "invalid-date",
+            "",
+            "9999-99-99T99:99:99.999Z"
+        )
+
+        return if (Random.nextBoolean()) {
+            validDates.random()
+        } else {
+            invalidDates.random()
+        }
     }
 }
